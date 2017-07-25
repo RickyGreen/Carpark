ec7ab470c7223c7b4bd90c08d57ef3ab33794e09
RickyGreen <RickyGreen@users.noreply.github.com>
2017-07-25 12:19:22 +0100

    namespace

--- charge.php
+++ charge.php
@@ -1,11 +1,11 @@
 <?php
   require_once(dirname(__FILE__) . '/config.php');
   $token  = $_POST['stripeToken'];
-  $customer = Stripe_Customer::create(array(
+  $customer = \Stripe\Stripe_Customer::create(array(
       'email' => 'customer@example.com',
       'card'  => $token
   ));
-  $charge = Stripe_Charge::create(array(
+  $charge = \Stripe\Stripe_Charge::create(array(
       'customer' => $customer->id,
       'amount'   => 50,
       'currency' => 'GBP'
