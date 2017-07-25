d001456870ccec08924c7e519bfdcb13d805b1ba
RickyGreen <RickyGreen@users.noreply.github.com>
2017-07-25 14:04:53 +0100

    Object to array
    Turned the card object to an array.

--- charge.php
+++ charge.php
@@ -11,15 +11,17 @@ $customer = \Stripe\Customer::create(array(
     'source'  => $token
 ));
 
-$customer = \Stripe\Customer::retrieve($customer->id);
-$customer->sources->create(array("source" => $card));
+$card = array(
+  $card->number = $_POST['card_number'],
+  $card->cvc = $_POST['cvc'],
+  $card->exp_month = $_POST['exp_month'],
+  $card->exp_year = $_POST['exp_month'],
+  $card->number = $_POST['number']
+);
 
-$card = new stdClass();
-$card->number = $_POST['card_number'];
-$card->cvc = $_POST['cvc'];
-$card->exp_month = $_POST['exp_month'];
-$card->exp_year = $_POST['exp_month'];
-$card->number = $_POST['number'];
+
+$customer = \Stripe\Customer::retrieve($customer->id);
+$customer->sources->create(array('source' => $card));
 
 $charge = \Stripe\Charge::create(array(
     'customer' => $customer->id,
